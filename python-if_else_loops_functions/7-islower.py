35a7b538e56a5bf140dfef1a0243cbed24df1709
mehdiicyber <13735@holbertonstudents.com>
2026-04-19 00:40:27 +0400

    Create 7-islower.py

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+def islower(c):
+    if 96 < ord(c) < 123:
+        return True
+    else:
+        return False
+if True:
+  print("{} is lower".format(c))
+else:
+  print("{} is upper".format(c))
+      

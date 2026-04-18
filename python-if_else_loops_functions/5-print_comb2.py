5e5166ffda34b342fd80cd59928ad426fcf8a5cf
mehdiicyber <13735@holbertonstudents.com>
2026-04-19 00:06:24 +0400

    Update 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-      print(f"0{i}, ", end = "")
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
     else:
-      print(f"{i}, ", end = "")
+        print("{:02d}".format(i))

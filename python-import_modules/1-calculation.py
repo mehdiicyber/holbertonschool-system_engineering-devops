25e48dd09e47c1e46e9204fae04bc731de5c15d1
mehdiicyber <13735@holbertonstudents.com>
2026-04-21 17:02:05 +0000

    Update

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from add_0 import add
+from add_0 import sub
+from add_0 import mul
+from add_0 import div
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} / {} = {}".format(a, b, div(a, b)))

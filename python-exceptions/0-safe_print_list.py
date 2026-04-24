945dccf810725980a75004741425073acb298d66
mehdiicyber <13735@holbertonstudents.com>
2026-04-24 07:13:01 +0000

    Update

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    """
+    Siyahıdakı x qədər elementi çap edən funksiya.
+    
+    Args:
+        my_list: Çap ediləcək elementlərin siyahısı.
+        x: Çap ediləcək elementlərin sayı.
+        
+    Returns:
+        Çap olunmuş elementlərin real sayı.
+    """
     count = 0
     for i in range(x):
         try:

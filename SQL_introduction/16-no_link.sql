ad15983afdfe91b6be5bbe5d74ac0ffb4793fa70
mehdiicyber <13735@holbertonstudents.com>
2026-07-02 01:05:59 +0400

    Delete SQL_introduction directory

--- SQL_introduction/16-no_link.sql
+++
@@ -1,5 +0,0 @@
--- Lists all records of second_table with a valid name, ordered by score (descending)
-SELECT score, name 
-FROM second_table 
-WHERE name IS NOT NULL AND name != ''
-ORDER BY score DESC;
